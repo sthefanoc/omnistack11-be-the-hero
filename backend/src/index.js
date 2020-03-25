f15d2c9e1bfa2dacd8492640d1f9f714e6aeb976
Sthefano Carvalho <sthefanocarvalho.consultoria@gmail.com>
2020-03-25 18:25:58 -0300

    backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const routes = require('./routes');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota/ Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * GET: Buscar info no back
+ * POST: criar info no back
+ * PUT: alterar info no back
+ * DELETE: detalar info no back
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query params
+  * Route params
+  * Request body: corpo da requisição utilizado para criar ou alterar recursos
+  */
+ 
+  /**
+   * Tipos de bancos de dado:
+   * 
+   * SQL: MySQL, SQLite, PostreSQL, Oracle, Miscrosoft SQL Server
+   * NoSQL: MondoDB, CouchDB, etc
+   * 
+   * Funciona de três formas principais:
+   * 
+   * Driver: SELECT * FROM users
+   * Query Builder: table('users').select('*').where()
+   */
+
+
+
+
+
+
