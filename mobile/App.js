01a219d6aae557711fcfb5067f8bc39b153a9333
Sthefano Carvalho <sthefanocarvalho.consultoria@gmail.com>
2020-03-27 11:13:43 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#7159c1',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+
+//   title: {
+//     color: '#FFF',
+//     fontSize: 20,
+//     fontWeight: 'bold'
+//   },
+// });
